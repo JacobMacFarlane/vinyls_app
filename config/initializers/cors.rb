ed06c99f058ac60e94d06f76059323a1a09374a6
Jacob MacFarlane <jmacfarlane@auddia.dev>
2023-12-19 09:24:54 -0700

    Update CORS settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'localhost:3000', '127.0.0.1:3000'
-  
+      origins 'localhost:3000', '127.0.0.1:3000', 'your-frontend-domain.com'
+      
       resource '*',
         headers: :any,
-        methods: [:get, :post, :patch, :put, :delete, :options, :head],
+        methods: [:get, :post, :put, :delete, :options, :head, :patch],
         credentials: true
     end
   end
